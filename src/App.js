7886d55f94342884c9ae1a656b21d3e9b5ba322a
Dabsia <Dabojohnson98@gmail.com>
2022-08-10 02:40:15 -0700

    Added the footer component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Carosel from './components/Carosel/Carosel';
 import Destinations from './components/Destinations/Destinations';
 import Details from './components/Details/Details';
+import Footer from './components/Footer/Footer';
 import Hero from './components/Hero/Hero';
 import ImagesList from './components/ImagesList/ImagesList';
 import NavBar from './components/NavBar/NavBar';
@@ -14,6 +15,7 @@ function App() {
       <Details />
       <ImagesList />
       <Carosel />
+      <Footer />
     </div>
   );
 }
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -36,7 +36,7 @@ const NavBar = () => {
           <HiOutlineMenuAlt4 size={20} onClick = {handleNav} />}
       </div>
       {/**Mobile Menu */}
-      <div onClick = {handleNav} className={ nav ? 'absolute left-0 [-100%] top-0 w-full bg-gray-100/90 px-4 py-7 text-black flex flex-col' : 'absolute left-[-100%]' }>
+      <div className={ nav ? 'absolute left-0 [-100%] top-0 w-full bg-gray-100/90 px-4 py-7 text-black flex flex-col' : 'absolute left-[-100%]' }>
         <ul >
           {nav && <h1>BEACHES.</h1>}
               <li className='border-b'>Home</li>
